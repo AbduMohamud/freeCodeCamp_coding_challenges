021383c3b18e25ef618ea07ff262e16aa838866c
AbduMohamud <mohamudabdu@gmail.com>
2025-12-06 18:14:33 -0500

    update

--- camel_to_snake.py
+++ camel_to_snake.py
@@ -6,13 +6,14 @@ Given a string in camel case, return the snake case version of the string using 
 The input string will contain only letters (A-Z and a-z) and will always start with a lowercase letter.
 Every uppercase letter in the camel case string starts a new word.
 Convert all letters to lowercase.
-Separate words with an underscore (_).  
+Separate words with an underscore (_). 
     
     """
 
 camel_1 = "helloWorld"
 camel_2 = "myVariableName"    
 camel_3 = "freecodecampDailyChallenges"
+camel_4 =  "freecodecampDailyChallengesDayDayDayDay"
 
 
 print(len(camel_1))
@@ -26,26 +27,36 @@ def to_snake (camel):
     snake_index = []
     snake_list = []
     snake_dict = {}
+    pos_counter = 0
     
     for i in range(len(camel)): 
         if camel[i].isupper():
-            print(i)
+            
+            print(100+ pos_counter)
             snake_index.append(i)
+            #camel = camel.strip(camel[snake_index[pos_counter]:i])
             camel_split = camel.split(camel[i:])
-            snake_list.append(camel[:i])
-            snake_list.append(camel[i:])
             
-            snake_dict[i] = camel[i]
+            snake_list.append(camel[snake_index[pos_counter]:len(camel) - i])
             
+            print(snake_index[pos_counter])
+            snake_dict[i] = camel[snake_dict[pos_counter]:i]
+            
+            pos_counter += 1  
+            print(pos_counter)  
             #new_snake = ''.join(snake)
       
             #snake_list.append(camel[:i])
+            
+    # make a list of the words split by the capital letter, 
+    # then join the list into a sting, replace the blank ' ' space with _
         
     
     
-    return snake_index, camel_split, snake_list, snake_dict
+    return snake_list, snake_index, snake_dict
     
 to_snake(camel_1)
 to_snake(camel_2)
 to_snake(camel_3)
 
+to_snake(camel_4)
